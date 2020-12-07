2cfdaec2a4b103c9cfb17916c4c0407a46c95035
KaiyuSugiyama <71801717+KaiyuSugiyama@users.noreply.github.com>
2020-12-06 20:37:41 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+
+	paper = new Paper(100,100,50);
+	bin = new DustBin(600, 700, 150,50);
+	binLeft = new DustBin(530, 680, 20, 200);
+	binRight = new DustBin(670, 680, 20, 200);
+
+	ground = new DustBin(400,700,900,20);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  paper.display();
+  bin.display();
+  binLeft.display();
+  binRight.display();
+  ground.display();
+
+  keyPressed();
+  drawSprites();
+  
+}
+function keyPressed() {
+	if(keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paper.body, paper.body.position, {x:3,y:-15});
+	}
+}
+
+
+
